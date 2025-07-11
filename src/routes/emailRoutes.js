01947ac774c09c8d7ebe26948706d507d365b1fc
Ganesh-D-Kumbhar <ganeshhh2003@gmail.com>
2025-07-11 16:55:05 +0530

    add wake-up route to email routes for server status check

--- src/routes/emailRoutes.js
+++ src/routes/emailRoutes.js
@@ -2,6 +2,10 @@ const express = require('express');
 const { contactForm } = require('../controllers/contactForm');
 
 const router = express.Router();
+// Wake-up route
+router.get('/wake-up', (req, res) => {
+  res.status(200).send('Server is awake!');
+});
 
 // Route to send email
 router.post('/contact-form', contactForm);
